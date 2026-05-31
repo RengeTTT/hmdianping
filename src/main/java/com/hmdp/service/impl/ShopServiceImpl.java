e6d87e9c28bfe4f297c053248d7b111c3c5a52c8
RengeTTT <2878666472@qq.com>
2026-05-31 23:50:18 +0800

    feat: 完成了互斥锁和逻辑过期解决缓存击穿解决热点key失效问题，同时编写缓存工具类

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,31 +1,42 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.CacheClient;
 import com.hmdp.utils.RedisConstants;
+import com.hmdp.utils.RedisData;
+import io.lettuce.core.json.JsonObject;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.StringUtils;
 
+
+import java.time.LocalDateTime;
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
  * @since 2021-12-22
  */
 @Service
+@Slf4j
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
 
     /**
@@ -33,31 +44,16 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
      */
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
+    @Autowired
+    private CacheClient cacheClient;
     @Override
     public Result queryById(Long id) {
         if (id == null) {
             return Result.fail("无效id");
         }
-        // 获取Redis商铺信息缓存
-        String cacheKey = RedisConstants.CACHE_SHOP_KEY + id;
-        String cache = stringRedisTemplate.opsForValue().get(cacheKey);
-        if (StrUtil.isNotBlank(cache)) {
-            // 如果存在缓存，那么直接返回
-            Shop cacheShop = JSONUtil.toBean(cache, Shop.class);
-            return Result.ok(cacheShop);
-        }
-        if (cache != null) {
-            return Result.fail("店铺不存在");
-        }
-        // 不存在缓存,查询数据库
-        Shop shop = this.getById(id);
-        if (shop == null) {
-            stringRedisTemplate.opsForValue().set(cacheKey, "", RedisConstants.CACHE_NULL_TTL, TimeUnit.MINUTES);
-            return Result.fail("数据库不存在该信息");
-        }
-        String key = RedisConstants.CACHE_SHOP_KEY + shop.getId();
-        Long TTL = new Random().nextLong(RedisConstants.CACHE_SHOP_TTL - 5 + 1) + 5;
-        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop), TTL, TimeUnit.MINUTES);
+        // Shop shop = queryWithPassThrough(id);
+        Shop shop = cacheClient.queryWithMutex(RedisConstants.CACHE_SHOP_KEY, Shop.class, id, this :: getById, RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        //Shop shop = cacheClient.queryWithLogicalExpire();
         return Result.ok(shop);
     }
 
@@ -76,4 +72,8 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         stringRedisTemplate.delete(cacheKey);
         return Result.ok();
     }
+
+
+
+
 }
