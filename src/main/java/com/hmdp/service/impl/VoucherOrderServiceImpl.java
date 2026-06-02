7fe3f0a567b17149aa0f00aca3f7fb29bef5834a
RengeTTT <2878666472@qq.com>
2026-06-02 23:48:01 +0800

    fix: 修复了秒杀抢购超卖超卖问题,利用CAS进行超卖限制，悲观锁进行超买限制

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -10,14 +10,16 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.utils.RedisConstants;
 import com.hmdp.utils.RedisIWorker;
 import com.hmdp.utils.UserHolder;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
@@ -30,8 +32,8 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     private ISeckillVoucherService seckillVoucherService;
     @Autowired
     private RedisIWorker redisIWorker;
+
     /**
-     *
      * @param voucherId
      * @return
      */
@@ -54,10 +56,30 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         if (seckillVoucher.getStock() < 1) {
             return Result.fail("库存不足");
         }
+        Long userId = UserHolder.getUser().getId();
+        synchronized (userId.toString().intern()) {
+            // 获取代理对象
+            IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
+            return proxy.createSeckillVoucher(voucherId);
+        }
+    }
+
+    @Transactional
+    public Result createSeckillVoucher(Long voucherId) {
+
+        Long userId = UserHolder.getUser().getId();
+        // 使用悲观锁解决用户超买
+        Long count = this.query().eq("user_id", userId).eq("voucher_id", voucherId).count();
+        if (count > 0) {
+            return Result.fail("同一用户只能购买一次!");
+        }
+        // 使用CAS解决库存超卖问题
         // 库存减少
         boolean success = seckillVoucherService.update()
                 .setSql("stock = stock - 1")
-                .eq("voucher_id", voucherId).update();
+                .eq("voucher_id", voucherId)
+                .gt("stock", 0) // 只要库存仍然大于0, 那么处于这个语句的线程就能继续执行
+                .update();
         if (!success) {
             return Result.fail("扣减失败");
         }
@@ -71,4 +93,5 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         save(voucherOrder);
         return Result.ok("秒杀成功");
     }
+
 }
