8088eb96fbfa0ef21cf214879796ec584f8605fc
RengeTTT <2878666472@qq.com>
2026-06-13 21:55:06 +0800

    feat: 使用Redis进行秒杀资格判断和库存预扣减，使用基于stream消息队列进行库存扣减，提高并发性能

--- src/main/resources/lua/seckill.lua
+++ src/main/resources/lua/seckill.lua
@@ -2,7 +2,7 @@
 -- 传入的参数
 local voucherId = argv[1]
 local userId = argv[2]
-
+local orderId = argv[3]
 local stockKey = "seckill:stock" .. voucherId
 local userKey = "seckill:order" .. voucherId
 if (tonumber(redis.call("get", stockKey) < 1)) then
@@ -16,4 +16,6 @@ end
 -- 秒杀成功，扣减库存，添加用户
 redis.call("incrby", stockKey, -1)
 redis.call("sadd", userKey, userId);
+-- 将订单信息加入到stream消息队列
+redis.call("xadd", "stream.orders", "*", "userId", userId, "orderId", orderId ,"voucherId", voucherId)
 return 0
